85acaa3ce781d7ebae7f2825d7142dae56d5b0a5
eunhoya <dmsgh1789@naver.com>
2022-04-22 19:19:14 +0900

    state 더 공부하기 expenseForm 끝

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -29,7 +30,7 @@ const App = () => {
   ];
   return (
     <div>
-      <h2>Expenses</h2>
+      <NewExpense></NewExpense>
       <Expenses items={expenses} />
     </div>
   );
