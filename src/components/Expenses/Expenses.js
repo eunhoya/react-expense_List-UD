9860c9308dfb43fab55ee224dc591f15d22068f1
EHEH <81672416+eunhoya@users.noreply.github.com>
2022-10-25 15:19:30 +0900

    정리

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,34 +1,35 @@
-import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
-import Card from "../UI/Card";
-import ExpensesFilter from "./ExpensesFilter";
-import "./Expenses.css";
-import "../UI/Card.css";
+import React, { useState } from 'react';
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
+import '../UI/Card.css';
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020");
+	const [filteredYear, setFilteredYear] = useState('2020');
 
-  const filterChangeHandler = (selectedYear) => {
-    setFilteredYear(selectedYear);
-  };
+	const filterChangeHandler = (selectedYear) => {
+		setFilteredYear(selectedYear);
+	};
 
-  return (
-    <div>
-      <Card className="expenses">
-        <ExpensesFilter
-          selected={filteredYear}
-          onChangeFilter={filterChangeHandler}
-        />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
-      </Card>
-    </div>
-  );
+	return (
+		<div>
+			<Card className='expenses'>
+				<ExpensesFilter
+					selected={filteredYear}
+					onChangeFilter={filterChangeHandler}
+				/>
+				{props.items.map((expense) => (
+					<ExpenseItem
+						key={expense.title}
+						title={expense.title}
+						amount={expense.amount}
+						date={expense.date}
+					/>
+				))}
+			</Card>
+		</div>
+	);
 };
 
 export default Expenses;
